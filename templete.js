be7074aca3a5845a853fa3f3f8d3b72ab46ea967
shindos <shindos@internet.ru>
2024-01-03 21:22:42 +0300

    start level 5

--- templete.js
+++ templete.js
@@ -15,3 +15,27 @@ console.log(template2)
 const templateMode2 = `project 
 price: ${price}$`
   console.log(templateMode2)
+ 
+// Convertion type
+const age = '18'
+console.log(Number(age) + 5)
+console.log(age - 3) // 15; Неявное преобразование типов.
+const userName = 'Vasya'
+console.log(Number(userName) + 5) // NaN
+console.log(typeof NaN) // Number
+console.log(String(4) + 7) // 47
+console.log(Boolean(0)) // false
+console.log(Boolean(-1)) // true, все числа кроме нуля true.
+console.log(Boolean('')) // false полезная фича
+console.log(Boolean('') + 10) // 10
+console.log(true + 2) // 3
+
+// Convertion Boolean
+// false
+
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(Number('Vasya')));
+console.log(Boolean(NaN));
