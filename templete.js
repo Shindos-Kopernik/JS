30d4e36c1bccc7eabc8cc5a7de465925f7a75088
shindos <shindos@internet.ru>
2024-01-03 20:31:28 +0300

    4 - 17 template done

--- templete.js
+++ templete.js
@@ -1,2 +1,17 @@
-const projectName = 'Site shop'
-const 
+// template and templateMode
+
+const projectName = 'site shop'
+const price = 2000
+const author = 'Vasiliy Petrov'
+const template = author + ' order ' + projectName + ' price $' + price + '$'
+console.log(template)
+
+const templateMode = `${author} order ${projectName} price ${price}$`
+console.log(templateMode)
+
+const template2 = 'project \n' + 'price: ' + price + '$';
+console.log(template2)
+
+const templateMode2 = `project 
+price: ${price}$`
+  console.log(templateMode2)
