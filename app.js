fcdfe1bfc20b7375884d725f003df49bcaa2ac9a
shindos <shindos@internet.ru>
2024-01-03 20:08:10 +0300

    4 - 17 template

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const name = 'Anton'
 console.log("hello world", name)
-name = 'ddd'
 console.log(name)
+console.log(
+  'shindos'
+)
+// template and templateMode
+
+const projectName = 'site shop'
+const price = 2000
+const author = 'Vasiliy Petrov'
+const template = author + ' order ' + projectName + ' price $' + price + '$'
+console.log(template)
+
+const templateMode = `${author} order ${projectName} price ${price}$`
+console.log(templateMode)
---
+++ ter.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+<div>
+<h1>fdsf</h1>
+<p>Count: {count}</p>
+<button onClick={sdfdf}>fdfd</button>
+</div>
+<div>
+ 
